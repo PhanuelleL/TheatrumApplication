98b23f64a0610497c0bc629d2c64e31b4c4443b6
Phanuelle <phanuellienou@gmail.com>
2025-03-07 16:51:05 -0500

    Creation de la page Contact et de le page Connexion

---
+++ components/Connexion.jsx
@@ -0,0 +1,41 @@
+export default function Connexion() {
+ return (
+  <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white p-6">
+   <h1 className="text-4xl font-bold text-yellow-400 mb-6 border-b-4 border-yellow-400 pb-2">
+    Connexion
+   </h1>
+
+   <div className="bg-black p-8 rounded-lg shadow-lg w-full max-w-md border">
+    <form className="grid grid-cols-1 gap-6">
+     <div>
+      <label className="block text-whitte font-semibold">Email</label>
+      <input
+       type="email"
+       placeholder="Entrer votre email"
+       className="w-full p-3 rounded bg-black border border-gold text-white focus:ring-2"
+      />
+     </div>
+
+     <div>
+      <label className="block text-gold font-semibold">Mot de passe</label>
+      <input
+       type="password"
+       placeholder="Votre mot de passe"
+       className="w-full p-3 rounded bg-black border border-gold text-white "
+      />
+     </div>
+
+     <button className="w-full yellow-400 hover:bg-gray-500 hover:text-black text-white py-3 rounded-lg font-bold transition-all duration-300">
+      Se connecter
+     </button>
+    </form>
+
+    <div className="mt-4 text-center text-white">
+     <a href="#" className="text-sm hover:text-bordeaux">
+      Mot de passe oublié ?
+     </a>
+    </div>
+   </div>
+  </div>
+ );
+}
---
+++ components/Contact.jsx
@@ -0,0 +1,127 @@
+"use client";
+import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
+import { use, useState } from "react";
+
+export default function Contact() {
+ const [result, setResult] = useState("");
+ const [success, setSuccess] = useState(true);
+
+ const {
+  register,
+  handleSubmit,
+  getValues,
+  formState: { errors },
+ } = useForm({
+  defaultValues: {
+   nom: "",
+   email: "",
+   objet: "",
+   message: "",
+  },
+ });
+
+ const sendMail = () => {
+  const values = getValues();
+  console.log(values);
+  const templateParams = {
+   name: values.nom,
+   email: values.email,
+   subject: values.objet,
+   message: values.message,
+  };
+
+  emailjs
+   .send(
+    "service_7eejsm2",
+    "template_8tkgbbw",
+    templateParams,
+    "4WP52q8KHhrUBeeF6"
+   )
+   .then(
+    (response) => {
+     setResult("Message envoyé avec succès !");
+     setSuccess(true);
+    },
+    (error) => {
+     setResult("Erreur lors de l'envoi du message.");
+     setSuccess(false);
+    }
+   )
+
+   .catch(() => alert);
+ };
+
+ return (
+  <div className="flex justify-center items-center bg-black min-h-screen border-yellow-400 text-white p-6">
+   <form
+    onSubmit={handleSubmit(sendMail)}
+    className="bg-black p-8 rounded-lg shadow-lg w-full max-w-lg border-2 border-yellow-400"
+   >
+    <h1 className="text-4xl font-bold text-yellow-400 mb-6 text-center border-b-4 border-yellow-400 pb-2">
+     Contactez-nous
+    </h1>
+
+    <label className="block text-white font-semibold mt-4">
+     Nom:
+     <input
+      {...register("nom", { required: "Champ obligatoire" })}
+      type="text"
+      className="w-full p-3 rounded bg-black border border-yellow text-white focus:ring-2 focus:ring-bordeaux"
+     />
+     {errors.nom && (
+      <span className="text-yellow-500 text-xs">{errors.nom.message}</span>
+     )}
+    </label>
+
+    <label className="block text-gold font-semibold mt-4">
+     Email:
+     <input
+      {...register("email", {
+       required: "Champ obligatoire",
+       pattern: {
+        value:
+         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        message: "Email invalide",
+       },
+      })}
+      type="text"
+      className="w-full p-3 rounded bg-black border border-gold text-white focus:ring-2 focus:ring-bordeaux"
+     />
+     {errors.email && (
+      <span className="text-yellow-500 text-xs">{errors.email.message}</span>
+     )}
+    </label>
+
+    <label className="block text-gold font-semibold mt-4">
+     Objet:
+     <input
+      {...register("objet")}
+      type="text"
+      className="w-full p-3 rounded bg-black border border-gold text-white focus:ring-2 focus:ring-bordeaux"
+     />
+    </label>
+
+    <label className="block text-gold font-semibold mt-4">Message:</label>
+    <textarea
+     {...register("message")}
+     className="w-full p-3 rounded bg-black border border-gold text-white focus:ring-2 focus:ring-bordeaux"
+     rows={5}
+    ></textarea>
+
+    <div className="flex justify-center gap-4 mt-6">
+     <input
+      type="submit"
+      value="Envoyer"
+      className="bg-gray-600 hover:bg-yellow-400 text-white py-3 px-6 rounded-lg "
+     ></input>
+     <input
+      type="button"
+      value="Annuler"
+      className="bg-gray-600 hover:bg-yellow-400 text-white py-3 px-6 rounded-lg"
+     ></input>
+    </div>
+   </form>
+  </div>
+ );
+}
